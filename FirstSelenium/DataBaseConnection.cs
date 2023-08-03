7695abb11525068acb0b8a38ccc0170693fb4f3e
dattatraySushir <sushirdattatray@gmail.com>
2023-08-03 15:59:17 +0530

    extent reports were added

---
+++ FirstSelenium/DataBaseConnection.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Data.Odbc;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstSelenium
+{
+    [TestClass]
+    public class DataBaseConnection
+    {
+        [TestMethod]
+        public void DataBaseConnectionTest()
+        {
+            OdbcConnection conn = new OdbcConnection();
+
+            //conn.ConnectionString =  $"jdbc:mysql://rmgtestingserver:3333/projects""root" };
+          
+        }
+    }
+}
--- FirstSelenium/ExtentReportsData.cs
+++ FirstSelenium/ExtentReportsData.cs
@@ -26,8 +26,33 @@ namespace FirstSelenium
 
            ExtentTest  extentTest= extentReports.CreateTest("Datta");
             extentTest.Log(Status.Info, "The report is being generated and status is updated");
+            extentTest.Log(Status.Warning,"This is the warning symbol");
+            extentTest.Log(Status.Fatal,"The test case is fatal");
             extentTest.Pass("The test case is pass");
             extentTest.Fail("The test case is fail");
+
+            ExtentTest extentTest1 = extentReports.CreateTest("Tejas");
+            extentTest1.Log(Status.Info, "The report is being generated and status is updated");
+            extentTest1.Log(Status.Warning, "This is the warning symbol for tejas");
+            extentTest1.Log(Status.Fatal, "Tejas test case is fatal");
+            extentTest1.Pass("The test case is pass tejas");
+            extentTest1.Fail("The test case is fail of tejas");
+
+            ExtentTest extentTest2 = extentReports.CreateTest("Veena");
+            extentTest2.Log(Status.Info, "The report is being generated and status is updated");
+            extentTest2.Log(Status.Warning, "This is the warning symbol for veena");
+            extentTest2.Log(Status.Fatal, "The test case is fatal for veena");
+            extentTest2.Pass("The test case is pass for veena");
+            extentTest2.Fail("The test case is fail for veena");
+
+            ExtentTest extentTest3 = extentReports.CreateTest("Sushma");
+            extentTest3.Log(Status.Info, "The report is being generated and status is updated");
+            extentTest3.Log(Status.Warning, "This is the warning symbol for sushma");
+            extentTest3.Log(Status.Fatal, "The test case is fatal sushma");
+            extentTest3.Pass("The test case is pass sushma");
+            extentTest3.Fail("The test case is fail sushma");
+
+
             extentReports.Flush();
             extentHtmlReporter.Stop();
             
